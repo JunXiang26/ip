7aa2b31158df886eaad9f5e5a1200453800fdb18
JunXiang26 <60207430+JunXiang26@users.noreply.github.com>
2025-01-23 14:52:45 +0800

    Level 1 - Echo

--- src/main/java/Dubey.java
+++ src/main/java/Dubey.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Dubey {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                    + "Hello! I'm Dubey!      \n"
-                    + "What can I do for you? \n"
-                    + "____________________________________________________________\n"
-                    + "Bye. Hope to see you again soon!\n"
-                    + "____________________________________________________________\n";
-        System.out.println(logo);
+        String intro = "     ____________________________________________________________\n"
+                    + "     Hello! I'm Dubey!      \n"
+                    + "     What can I do for you? \n"
+                    + "     ____________________________________________________________\n";
+        System.out.println(intro);
 
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println( "     ____________________________________________________________\n" +
+                                "       " + input + "\n" +
+                                "     ____________________________________________________________\n"
+            );
+            input = scanner.nextLine();
+        }
+
+        String bye = "     ____________________________________________________________\n"
+                    + "      Bye. Hope to see you again soon!\n"
+                    + "     ____________________________________________________________\n";
+        System.out.println(bye);
     }
 }
