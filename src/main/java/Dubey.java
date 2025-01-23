f8801ca4e32686e49e3a062cd2d93b3226274d63
JunXiang26 <60207430+JunXiang26@users.noreply.github.com>
2025-01-23 16:57:45 +0800

    Level 4 - ToDos, Events, Deadlines

--- src/main/java/Dubey.java
+++ src/main/java/Dubey.java
@@ -1,28 +1,73 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Dubey {
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
+class Task {
+    protected String description;
+    protected boolean isDone;
 
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-
-        public void setStatus(boolean status) {
-            isDone = status;
-        }
-
-        public String getDescription() {
-            return description;
-        }
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
     }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setStatus(boolean status) {
+        isDone = status;
+    }
+
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
+    }
+}
+
+class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+    }
+}
+
+public class Dubey {
 
     public static void main(String[] args) {
         ArrayList<Task> taskList = new ArrayList<>(100);
@@ -42,10 +87,9 @@ public class Dubey {
                 case "list":
                     System.out.println("     ____________________________________________________________\n"
                                      + "      Here are the tasks in your list:\n");
-                    int i = 1;
                     for (Task task : taskList) {
-                        System.out.println("      " + i + ".[" + task.getStatusIcon() + "] " + task.getDescription());
-                        i++;
+                        int i = taskList.indexOf(task) + 1;
+                        System.out.println("      " + i + "." + task.toString());
                     }
                     System.out.println("     ____________________________________________________________\n");
                     break;
@@ -55,7 +99,7 @@ public class Dubey {
                     taskList.get(markIndex - 1).setStatus(true);
                     String marked = "     ____________________________________________________________\n"
                             + "      Nice! I've marked this task as done:\n"
-                            + "        [" + taskList.get(markIndex - 1).getStatusIcon() + "] " + taskList.get(markIndex - 1).getDescription()+ "\n"
+                            + "         " + taskList.get(markIndex - 1).toString() + "\n"
                             + "     ____________________________________________________________\n";
                     System.out.println(marked);
                     break;
@@ -65,9 +109,42 @@ public class Dubey {
                     taskList.get(unmarkIndex - 1).setStatus(false);
                     String unmarked = "     ____________________________________________________________\n"
                             + "      OK, I've marked this task as not done yet:\n"
-                            + "        [" + taskList.get(unmarkIndex - 1).getStatusIcon() + "] " + taskList.get(unmarkIndex - 1).getDescription()+ "\n"
+                            + "         " + taskList.get(unmarkIndex - 1).toString() + "\n"
                             + "     ____________________________________________________________\n";
                     System.out.println(unmarked);
+                    break;
+                case "todo":
+                    String descriptionTodo = input.substring(4);
+                    taskList.add(new Todo(descriptionTodo));
+                    System.out.println("     ____________________________________________________________\n" +
+                            "      Got it. I've added this task:\n" +
+                            "        " + taskList.getLast().toString() + "\n" +
+                            "      Now you have " + taskList.size() + " tasks in the list.\n" +
+                            "     ____________________________________________________________\n"
+                    );
+                    break;
+                case "deadline":
+                    String descriptionDeadline = input.substring(8, input.indexOf("/"));
+                    String deadline = input.substring(input.indexOf("/") + 4);
+                    taskList.add(new Deadline(descriptionDeadline,  deadline));
+                    System.out.println("     ____________________________________________________________\n" +
+                            "      Got it. I've added this task:\n" +
+                            "        " + taskList.getLast().toString() + "\n" +
+                            "      Now you have " + taskList.size() + " tasks in the list.\n" +
+                            "     ____________________________________________________________\n"
+                    );
+                    break;
+                case "event":
+                    String descriptionEvent = input.substring(5, input.indexOf("/"));
+                    String eventFrom = input.substring(input.indexOf("/from") + 6, input.indexOf("/to"));
+                    String eventTo = input.substring(input.indexOf("/to") + 4);
+                    taskList.add(new Event(descriptionEvent,  eventFrom, eventTo));
+                    System.out.println("     ____________________________________________________________\n" +
+                            "      Got it. I've added this task:\n" +
+                            "        " + taskList.getLast().toString() + "\n" +
+                            "      Now you have " + taskList.size() + " tasks in the list.\n" +
+                            "     ____________________________________________________________\n"
+                    );
                     break;
                 default:
                     taskList.add(new Task(input));
