8c3ff752eedabe9347ca89308cd55e0f06a9de97
JunXiang26 <60207430+JunXiang26@users.noreply.github.com>
2025-01-23 14:34:35 +0800

    Level 0 - Rename, Greet, Exit

---
+++ src/main/java/Dubey.java
@@ -0,0 +1,12 @@
+public class Dubey {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                    + "Hello! I'm Dubey!      \n"
+                    + "What can I do for you? \n"
+                    + "____________________________________________________________\n"
+                    + "Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________\n";
+        System.out.println(logo);
+
+    }
+}
